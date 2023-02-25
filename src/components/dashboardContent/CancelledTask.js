192d98f39192317bdcc767567d504c3e9a9af517
Asif Ferdous <asifferdous23@gmail.com>
2023-02-25 23:55:02 +0600

    final

--- src/components/dashboardContent/CancelledTask.js
+++ src/components/dashboardContent/CancelledTask.js
@@ -31,26 +31,6 @@ const CancelledTask = () => {
         <div className="col-12 col-md-3 col-lg-3 px-3">
           <h5>Task Cancelled</h5>
         </div>
-        <div className="col-12 col-md-3 col-lg-5 px-3">
-          <div className="row">
-            <div className="col-8">
-              <input type="date" className="form-control" />
-            </div>
-            <div className="col-4">
-              <button className="btn btn-primary">Search</button>
-            </div>
-          </div>
-        </div>
-        <div className="col-12 float-end col-md-4 col-lg-4 px-2">
-          <div className="row">
-            <div className="col-8">
-              <input className="form-control w-100" />
-            </div>
-            <div className="col-4">
-              <button className="btn btn-primary w-100">Search</button>
-            </div>
-          </div>
-        </div>
       </div>
       <div className="row p-0 m-0">
       <div className="col-12 col-lg-4 col-sm-6 col-md-4  p-2">
--- src/components/dashboardContent/NewTask.js
+++ src/components/dashboardContent/NewTask.js
@@ -31,26 +31,8 @@ const NewTask = () => {
         <div className="col-12 col-md-3 col-lg-3 px-3">
           <h5>New Task</h5>
         </div>
-        <div className="col-12 col-md-3 col-lg-5 px-3">
-          <div className="row">
-            <div className="col-8">
-              <input type="date" className="form-control" />
-            </div>
-            <div className="col-4">
-              <button className="btn btn-primary">Search</button>
-            </div>
-          </div>
-        </div>
-        <div className="col-12 float-end col-md-4 col-lg-4 px-2">
-          <div className="row">
-            <div className="col-8">
-              <input className="form-control w-100" />
-            </div>
-            <div className="col-4">
-              <button className="btn btn-primary w-100">Search</button>
-            </div>
-          </div>
-        </div>
+
+        
       </div>
 
       <div className="row p-0 m-0">
